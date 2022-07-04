4b75d8cacf4a396144dd23052b434c1605287adc
jacoposergio <djago32@gmail.com>
2022-07-04 16:42:43 +0200

    start

--- js/script.js
+++ js/script.js
@@ -10,11 +10,16 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            emailsArray:[]
         },
         methods: {
         },
         mounted() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+               this.emailsArray = response.data.response; 
+                
+            });
            
         }
     }
